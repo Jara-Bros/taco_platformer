6c3e13fbeb6ef6b14c110786037ae8fdebe44fcd
Nico Jara <nicojaramillo98@gmail.com>
2024-06-15 07:43:46 -0700

    taco proto commited to taco platformer

---
+++ taco/taco.gd
@@ -0,0 +1,59 @@
+extends CharacterBody2D
+
+
+@export var speed : int
+@export var jump_velocity : int
+@export var gravity : int = 980
+
+@onready var sprite_2d = $Sprite2D
+@onready var animation_player = $AnimationPlayer
+
+
+func _physics_process(delta):
+	
+	var direction = Input.get_axis("move_left", "move_right")
+	var acceleration : int = 1
+	
+	# For walk and run speeds
+	velocity.x += speed * delta
+
+		
+		
+	if direction:
+		velocity.x = direction * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+
+	move_and_slide()
+	
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += 1.5 * gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_velocity
+	
+	if Input.is_action_just_released("jump") and velocity.y < 0:
+		velocity.y = jump_velocity / 2
+	
+	
+	# Flip sprite
+	if direction > 0:
+		sprite_2d.flip_h = false
+	elif direction < 0:
+		sprite_2d.flip_h = true
+	
+	# Play animations
+	if is_on_floor():
+		if direction == 0:
+			animation_player.play("idle")
+		else:
+			animation_player.play("walk")
+	else:
+		if velocity.y < 0:
+			animation_player.play("jump")
+		if velocity.y > 0:
+			animation_player.play("idle")
+	
