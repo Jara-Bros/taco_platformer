650e8aaabf022b659a186f56b19c2bc425660590
Nico Jara <nicojaramillo98@gmail.com>
2024-07-12 09:03:40 -0700

    plaintain boat sprite, river adds

--- levels/river/parallax_2d.gd
+++ levels/river/parallax_2d.gd
@@ -1,7 +1,8 @@
 extends Parallax2D
 
-@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var forest_bg: Sprite2D = $ForestBG
+
 
 
 func _ready() -> void:
-	repeat_size = Vector2(sprite_2d.texture.get_width(), sprite_2d.texture.get_height())
+	repeat_size = Vector2(forest_bg.texture.get_width(), forest_bg.texture.get_height())
