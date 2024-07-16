193494c9998b3237202b00874752226ad6a19fec
Nico Jara <nicojaramillo98@gmail.com>
2024-07-15 19:47:57 -0700

    jungle add & waterfall

--- levels/river/river.gd
+++ levels/river/river.gd
@@ -1,22 +1,21 @@
 extends Node2D
 
-@onready var camera_2d: Camera2D = $Taco/Camera2D
 @onready var animation_player: AnimationPlayer = $CanvasLayer/AnimationPlayer
 @onready var taco: Player = $Taco
-@onready var river_slow_bm: AudioStreamPlayer = $RiverSlowBM
 @onready var music_transition: AudioStreamPlayer2D = $MusicTransition
 @onready var wing_flap_fx: AudioStreamPlayer2D = $WingFlapFX
+@onready var camera_2d: Camera2D = $Taco/Camera2D
+
 
 
 func _ready() -> void:
-	camera_2d.set_limit(SIDE_RIGHT, 855)
-	camera_2d.set_limit(SIDE_TOP, 245)
-	camera_2d.set_limit(SIDE_LEFT, 110)
-	
+	camera_2d.set_limit(SIDE_TOP, -260)
+	camera_2d.set_limit(SIDE_RIGHT, 830)
 	# For level transition
-	taco.input_enabled = false
-	music_transition.play()
-	wing_flap_fx.play()
-	animation_player.play_backwards("fade_in")
-	await animation_player.animation_finished
-	taco.input_enabled = true
+	
+	# taco.input_enabled = false
+	#music_transition.play()
+	#wing_flap_fx.play()
+	#animation_player.play_backwards("fade_in")
+	#await animation_player.animation_finished
+	#taco.input_enabled = true
