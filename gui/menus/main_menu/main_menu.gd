67f89256790c82cda42b2dbbd2b8c2fc430317d7
Nico Jara <nicojaramillo98@gmail.com>
2024-08-23 19:15:35 -0700

    updated main menu

--- gui/menus/main_menu/main_menu.gd
+++ gui/menus/main_menu/main_menu.gd
@@ -1,5 +1,20 @@
 extends Control
 
+@onready var button_v_box: VBoxContainer = $MarginContainer/ButtonVBox
 
-func _on_play_button_pressed() -> void:
+func _ready() -> void:
+	focus_button()
+
+
+func _on_start_button_pressed() -> void:
 	SceneManager.set_initial_scene("DiosMio")
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
+
+func focus_button() -> void:
+	if button_v_box:
+		var button: Button = button_v_box.get_child(0)
+		if button is Button:
+			button.grab_focus()
