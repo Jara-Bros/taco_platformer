bac13201ee713fbadf19c0fb8483ab2d8beaf519
Nico Jara <nicojaramillo98@gmail.com>
2024-09-20 21:42:35 -0700

    dialogic 2 update

--- addons/dialogic/Modules/Character/DefaultAnimations/slide_down_in_out.gd
+++ addons/dialogic/Modules/Character/DefaultAnimations/slide_down_in_out.gd
@@ -4,12 +4,12 @@ func animate() -> void:
 	var tween := (node.create_tween() as Tween)
 	tween.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
 
-	var target_position := end_position.y
+	var target_position := base_position.y
 	var start_position: float = -node.get_viewport().size.y
 
 	if is_reversed:
 		target_position = -node.get_viewport().size.y
-		start_position = end_position.y
+		start_position = base_position.y
 
 	node.position.y = start_position
 
@@ -17,3 +17,10 @@ func animate() -> void:
 
 	await tween.finished
 	finished_once.emit()
+
+
+func _get_named_variations() -> Dictionary:
+	return {
+		"slide in down": {"reversed": false, "type": AnimationType.IN},
+		"slide out up": {"reversed": true, "type": AnimationType.OUT},
+	}
--- levels/lunch_rush_level/lunch_rush_animation.gd
+++ levels/lunch_rush_level/lunch_rush_animation.gd
@@ -5,15 +5,18 @@ extends Node2D
 @onready var prep_sign: Sprite2D = $BG/PrepSign
 @onready var restaurant_wall: TextureRect = $BG/RestaurantWall
 @onready var host_wall: TextureRect = $Front/HostWall
-@onready var host_stand: Sprite2D = $Front/HostStand
-
+@onready var host_stand: Sprite2D = $Front/HostWall/HostStand
 
 
 func _ready() -> void:
+	Dialogic.start_timeline("ezzy_host_timeline")
+	host_wall.visible = true
+	host_stand.visible = true
+	await Dialogic.timeline_ended
+	
 	Dialogic.signal_event.connect(_on_dialogic_signal)
 	start_timer()
 	
-
 
 func start_timer():
 	await get_tree().create_timer(3.0).timeout
@@ -22,16 +25,8 @@ func start_timer():
 	await Dialogic.timeline_ended
 
 
+
 func _on_dialogic_signal(argument:String):
 	if argument == "switch_on_signs":
 		clock.frame = 1
 		prep_sign.frame = 1
-
-	if argument == "switch_to_front":
-		clock.visible = false
-		prep_sign.visible = false
-		restaurant_wall.visible = false
-		host_wall.visible = true
-		host_stand.visible = true
-		Dialogic.start_timeline("ezzy_host_timeline")
-		await Dialogic.timeline_ended
