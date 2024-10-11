6067bb9f094dc9ac1ab250229fe87fbe26b87a3c
Nico Jara <nicojaramillo98@gmail.com>
2024-10-10 21:59:30 -0700

    fall_platform adds

--- levels/lunch_rush_level/hungry_mob_boss_p_1.gd
+++ levels/lunch_rush_level/hungry_mob_boss_p_1.gd
@@ -1,19 +1,19 @@
 extends Node2D
 
-@onready var ray_cast: RayCast2D = $Path2D/PathFollow2D/AnimatableBody2D/RayCast2D
-@onready var anim_player: AnimationPlayer = $Path2D/AnimationPlayer
+@onready var anim_player: AnimationPlayer = $FallPlatform/Path2D/PathFollow2D/AnimatableBody2D/AnimationPlayer
+@onready var ray_cast: RayCast2D = $FallPlatform/Path2D/PathFollow2D/AnimatableBody2D/RayCast2D
+@onready var anim_player_2: AnimationPlayer = $FallPlatform/Path2D2/PathFollow2D/AnimatableBody2D/AnimationPlayer
+@onready var ray_cast_2: RayCast2D = $FallPlatform/Path2D2/PathFollow2D/AnimatableBody2D/RayCast2D
 
 
 func _ready() -> void:
 	pass
 
 
-
-func _process(delta: float) -> void:
-	
-	
+func _process(_delta: float) -> void:
 	
 	# Trigger falling platforms
-	# TODO: Set timer to delay falling plat
 	if ray_cast.is_colliding():
-		anim_player.play("move_block")
+			anim_player.play("fall_block")
+	if ray_cast_2.is_colliding():
+		anim_player_2.play("fall_block")
