efa88c31c2b4884dcb15954b60fc129e367c8f4f
Nico Jara <nicojaramillo98@gmail.com>
2024-12-22 09:18:10 -0500

    mr p & geff talk adds

--- gui/menus/main_menu/main_menu.gd
+++ gui/menus/main_menu/main_menu.gd
@@ -18,6 +18,7 @@ func _on_start_button_pressed() -> void:
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
 
+
 func focus_button() -> void:
 	if button_v_box:
 		var button: Button = button_v_box.get_child(0)
--- levels/salad_dream/kitchen_sequence.gd
+++ levels/salad_dream/kitchen_sequence.gd
@@ -1,10 +1,5 @@
 extends Node2D
 
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var manager: Sprite2D = $Manager
-@onready var jeff: Sprite2D = $Jeff
-@onready var act_transition: AnimationPlayer = $ActTransition
-
 
 var data = null
 
@@ -12,32 +7,7 @@ var data = null
 func _ready() -> void:
 	pass
 	
-	Dialogic.start("manager_timeline")
-	await Dialogic.timeline_ended
-	animation_player.play("black_off")
-	await animation_player.animation_finished
-	animation_player.play("kitchen_line_off")
-	await animation_player.animation_finished
-	Dialogic.start("manager_20_timeline")
-	animation_player.play("manager_talk_right")
-	animation_player.play("empanada_move")
-	await Dialogic.timeline_ended
-	Dialogic.start("manager_2_timeline")
-	animation_player.play("jeff_manager_talk")
-	await Dialogic.timeline_ended
-	Dialogic.start("jeff_timeline")
-	animation_player.play("jeff_talk")
-	await Dialogic.timeline_ended
-	Dialogic.start("manager_3_timeline")
-	animation_player.play("jeff_manager_talk")
-	await Dialogic.timeline_ended
-	Dialogic.start("jeff_2_timeline")
-	animation_player.play("sock_reveal")
-	await Dialogic.timeline_ended
-	animation_player.play("manager_irritated")
-	await animation_player.animation_finished
-	SceneManager.set_initial_scene("KitchenLineSequence")
-
 
 func dialogic_conversation():
 	Dialogic.start("kitchen_sequence")
+	await Dialogic.timeline_ended
