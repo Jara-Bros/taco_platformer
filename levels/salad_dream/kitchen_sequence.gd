88ea28171f8afe642078463946d7475b09a3645f
Nico Jara <nicojaramillo98@gmail.com>
2024-12-22 13:22:24 -0500

    kitchen sequence timeline mods

--- levels/salad_dream/kitchen_sequence.gd
+++ levels/salad_dream/kitchen_sequence.gd
@@ -1,13 +1,18 @@
 extends Node2D
 
+@onready var empanada_anim: AnimationPlayer = $EmpanadaAnimation
+@onready var empanada_1: AnimatedSprite2D = $Empanada1
+
 
 var data = null
 
 
 func _ready() -> void:
-	pass
+	empanada_1.visible = false
 	
 
 func dialogic_conversation():
 	Dialogic.start("kitchen_sequence")
+	empanada_1.visible = true
+	empanada_anim.play("empanada_move")
 	await Dialogic.timeline_ended
