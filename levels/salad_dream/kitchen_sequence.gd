66bfdfffeef590704947a3ca9dc2e2466b9f3f0d
Nico Jara <nicojaramillo98@gmail.com>
2025-01-02 15:36:18 -0500

    kitchen sequence adds

--- levels/salad_dream/kitchen_sequence.gd
+++ levels/salad_dream/kitchen_sequence.gd
@@ -1,19 +1,36 @@
 extends Node2D
 
-@onready var empanada_anim: AnimationPlayer = $EmpanadaAnimation
-@onready var empanada_1: AnimatedSprite2D = $Empanada1
+
+@onready var act_card: ColorRect = $ActCard
+@onready var mr_p_timer: Timer = $Timer
+@onready var mr_p_kitchen: TextureRect = $MrPKitchen
+@onready var empanada_anim_player: AnimationPlayer = $AnimatedSprite2D/AnimationPlayer
+@onready var geff_socks: TextureRect = $GeffSocks
 
 
 var data = null
 
 
 func _ready() -> void:
-	empanada_1.visible = false
-	
+	pass
+
 
 func dialogic_conversation():
+	Dialogic.start("mr_p_initial_talk")
+	await Dialogic.timeline_ended
+	act_card.visible = false
+	mr_p_timer.start()
+
+
+func _on_timer_timeout() -> void:
 	Dialogic.start("kitchen_sequence")
-	empanada_1.visible = true
-	empanada_anim.play("empanada_move")
+	mr_p_kitchen.visible = false
+	empanada_anim_player.play("empanada_move")
+	await Dialogic.timeline_ended
+	geff_socks.visible = true
+	Dialogic.start("kitchen_sequence_two")
+	await Dialogic.timeline_ended
+	geff_socks.visible = false
+	Dialogic.start("kitchen_sequence_three")
 	await Dialogic.timeline_ended
 	SceneManager.switch_scene("KitchenLineSequence", {})
