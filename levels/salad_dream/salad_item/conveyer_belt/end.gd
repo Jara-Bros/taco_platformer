aa2874bf994c601249319ebe7b1c46677bba3b1d
Andreas Jaramillo <89302526+andyjaramillo@users.noreply.github.com>
2025-01-02 18:34:42 -0500

    bug fixing with salad items

--- levels/salad_dream/salad_item/conveyer_belt/end.gd
+++ levels/salad_dream/salad_item/conveyer_belt/end.gd
@@ -31,12 +31,16 @@ func _on_area_entered(area):
 		
 		area.reset_bowl()
 		get_tree().current_scene.add_child(hud)
+		
+
 
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		$Label.visible = true
-
+	
+	if body.is_in_group("salad_item"):
+		body.queue_free()
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("Player"):
