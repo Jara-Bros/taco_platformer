74ecfdaaf276e2269be313725563272df5e38fcb
Nico Jara <nicojaramillo98@gmail.com>
2025-01-02 18:35:32 -0500

    sprites & label adds

--- levels/salad_dream/test_salad_dream.gd
+++ levels/salad_dream/test_salad_dream.gd
@@ -3,9 +3,16 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	hint_pause()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
+
+func hint_pause():
+	get_tree().paused = true
+	Dialogic.start("salad_dream_instruction")
+	Dialogic.timeline_ended
+	get_tree().paused = false
+	
