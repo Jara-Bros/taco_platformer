ccbb25e4d308dc3a8a90d2a3e2e0281e14240482
Nico Jara <nicojaramillo98@gmail.com>
2025-01-02 10:33:48 -0500

    main menu adds

--- gui/menus/main_menu/main_menu.gd
+++ gui/menus/main_menu/main_menu.gd
@@ -1,8 +1,13 @@
 extends Control
 
-@onready var button_v_box: VBoxContainer = $MarginMainMenuContainer/ButtonVBox
+@onready var button_v_box: VBoxContainer = $ColorRect/MarginMainMenuContainer/ButtonVBox
 @onready var anim_player: AnimationPlayer = $CanvasLayer/AnimationPlayer
 @onready var timer: Timer = $Timer
+
+@onready var main_face: Sprite2D = $MainFace
+@onready var main_face_two: Sprite2D = $MainFaceTwo
+@onready var main_face_three: Sprite2D = $MainFaceThree
+@onready var main_face_four: Sprite2D = $MainFaceFour
 
 
 func _ready() -> void:
@@ -28,3 +33,17 @@ func focus_button() -> void:
 
 func _on_timer_timeout() -> void:
 	focus_button()
+
+
+func face_change() -> void:
+	var new_frame = randi_range(0,7)
+	main_face.frame = new_frame
+	
+	var new_frame_2 = randi_range(0,7)
+	main_face_two.frame = new_frame_2
+
+	var new_frame_3 = randi_range(0,7)
+	main_face_three.frame = new_frame_3
+	
+	var new_frame_4 = randi_range(0,7)
+	main_face_four.frame = new_frame_4
