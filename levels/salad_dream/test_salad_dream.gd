e86ffa86d63aacf1c9e32c48c0d88f1a4309ac5f
Nico Jara <nicojaramillo98@gmail.com>
2025-01-03 08:45:10 -0500

    sprite changes & level moves

--- levels/salad_dream/test_salad_dream.gd
+++ levels/salad_dream/test_salad_dream.gd
@@ -3,16 +3,9 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	hint_pause()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
-
-func hint_pause():
-	get_tree().paused = true
-	Dialogic.start("salad_dream_instruction")
-	Dialogic.timeline_ended
-	get_tree().paused = false
-	
