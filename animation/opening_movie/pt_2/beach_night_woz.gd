cda8bae77067f5f9e250cf3818b18228e2d3936b
Nico Jara <nicojaramillo98@gmail.com>
2025-01-17 10:49:38 -0500

    beach night woz sprite adds

--- animation/opening_movie/pt_2/beach_night_woz.gd
+++ animation/opening_movie/pt_2/beach_night_woz.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
+
 @onready var woz: Sprite2D = $Woz
+@onready var anim_player: AnimationPlayer = $AnimationPlayer
 
 
 # Called when the node enters the scene tree for the first time.
@@ -9,10 +11,19 @@ func _ready() -> void:
 
 
 func _on_dialogic_signal(argument:String):
-	if argument == "change_frame":
+	if argument == "change_frame_one":
 		woz.frame = 1
-
-
+	if argument == "change_frame_two":
+		woz.frame = 2
+	if argument == "play_corpo_box":
+		anim_player.play("corpo_box")
+	
+	
 func play_woz_dialogue():
 	Dialogic.start("intro_beach")
 	await Dialogic.timeline_ended
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
