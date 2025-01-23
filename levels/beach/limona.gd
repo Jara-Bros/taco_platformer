a0ee700e096468ac7f0a6c1bdf1b38b7f658719f
Nico Jara <nicojaramillo98@gmail.com>
2025-01-23 17:35:02 -0500

    salad dream label fix

--- levels/beach/limona.gd
+++ levels/beach/limona.gd
@@ -1,6 +1,7 @@
 extends Sprite2D
 
 @onready var interaction_area: InteractionArea = $InteractionArea
+@onready var taco: Player = $"../Taco"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,8 +9,10 @@ func _ready() -> void:
 
 
 func _on_interact():
+	taco.input_enabled = false
 	Dialogic.start("limona_race_over")
 	await Dialogic.timeline_ended
+	taco.input_enabled = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
