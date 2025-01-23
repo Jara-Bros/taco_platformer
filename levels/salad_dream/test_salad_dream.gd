faf4a0cbc9d1346a3135810af14af9141b8b7a7c
Nico Jara <nicojaramillo98@gmail.com>
2025-01-23 17:11:49 -0500

    salad dream dialogue input fix

--- levels/salad_dream/test_salad_dream.gd
+++ levels/salad_dream/test_salad_dream.gd
@@ -1,11 +1,14 @@
 extends Node2D
 
 
+@onready var taco: Player = $Taco
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	taco.input_enabled = false
 	Dialogic.start("sous_salad_hint")
 	await Dialogic.timeline_ended
+	taco.input_enabled = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
