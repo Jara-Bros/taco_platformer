0bb436aa7592f391d3e25d2ae92248d5896a34b6
Nico Jara <nicojaramillo98@gmail.com>
2025-01-25 14:55:11 -0500

    interaction label fix

--- NPC/scripts/bleu_trigger.gd
+++ NPC/scripts/bleu_trigger.gd
@@ -1,16 +1,15 @@
 extends Node2D
 
+
 @onready var interaction_area: InteractionArea = $InteractionArea
-@onready var canvas_layer: CanvasLayer = $"../CanvasLayer"
-@onready var timer: Timer = $Timer
 @onready var taco: Player = $"../Taco"
 
 
 func _ready():
 	interaction_area.interact = Callable(self, "_on_interact")
 
+
 func _on_interact():
-	taco.input_enabled = false
 	Dialogic.start("taco_finds_bleu_timeline")
 	await Dialogic.timeline_ended
 	SceneManager.switch_scene("ThanksPlaying", {})
